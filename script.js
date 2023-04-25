3b9ee0874521e2e105033b2e90abc4748dd8bbd6
diorm47 <dior47m@gmail.com>
2023-04-25 19:27:09 +0500

    deploy

---
+++ script.js
@@ -0,0 +1,21 @@
+var advantage = document.querySelector(".advantage_block");
+var scroll_to_advantage = document.querySelector(".scroll_to_advantage");
+function scrollToAdvantage() {
+  advantage.scrollIntoView({ behavior: "smooth" });
+}
+scroll_to_advantage.addEventListener("click", scrollToAdvantage);
+
+var new_road = document.querySelector(".pre_footer");
+var scroll_to_new_road = document.querySelector(".scroll_to_new_road");
+function scrollToNewRoad() {
+  new_road.scrollIntoView({ behavior: "smooth" });
+}
+scroll_to_new_road.addEventListener("click", scrollToNewRoad);
+
+var footer_wrapper = document.querySelector(".footer_wrapper");
+var footer_wrapper_btn = document.querySelector(".scroll_to_footer_wrapper");
+function scrollToNewRoad() {
+  footer_wrapper.scrollIntoView({ behavior: "smooth" });
+}
+
+footer_wrapper_btn.addEventListener("click", scrollToNewRoad);
